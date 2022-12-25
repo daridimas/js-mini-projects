d42916e07b1140edf52fcdbc2f2a515f474d01e9
daridimas <inboxdimas@gmail.com>
2022-12-25 19:39:11 +0700

    otak atik inner html

---
+++ mini-project-1/starter/app.js
@@ -0,0 +1,7 @@
+const heading = document.querySelector(".heading-title")
+const badgelevel = document.querySelector(".badge")
+const footer = document.querySelector(".card-footer")
+
+heading.innerHTML = "Hello World"
+badgelevel.innerHTML = "Intermediate"
+footer.innerHTML = "Copyright ©️ 2022 Dimas😜"
