30fff1bfa73187f996a641b9873c0d99fc161d90
daridimas <inboxdimas@gmail.com>
2022-12-25 19:55:38 +0700

    text content

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -1,3 +1,4 @@
+// selector class
 const heading = document.querySelector(".heading-title")
 const badgelevel = document.querySelector(".badge")
 const footer = document.querySelector(".card-footer")
@@ -5,3 +6,7 @@ const footer = document.querySelector(".card-footer")
 heading.innerHTML = "Hello World"
 badgelevel.innerHTML = "Intermediate"
 footer.innerHTML = "Copyright ©️ 2022 Dimas😜"
+
+// selector ID
+const projectTitle = document.querySelector("#project-title")
+projectTitle.textContent = "Title Baru dari JS File"
